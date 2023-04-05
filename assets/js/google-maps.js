ea4414ff2e3322db63d6f5e0177553fa96a65c57
Manthan Vaghani <manthanvaghani4@gmail.com>
2023-04-05 16:07:36 +0530

    Init Commit

--- assets/js/google-maps.js
+++ assets/js/google-maps.js
@@ -1,15 +1,15 @@
 function initMap() {
-    // Latitude and Longitude
-    var myLatLng = {lat: -6.207690, lng: 106.985270};
+  // Latitude and Longitude
+  var myLatLng = { lat: 21.234871, lng: 72.870106 };
 
-    var map = new google.maps.Map(document.getElementById('google-maps'), {
-        zoom: 17,
-        center: myLatLng
-    });
+  var map = new google.maps.Map(document.getElementById("google-maps"), {
+    zoom: 17,
+    center: myLatLng,
+  });
 
-    var marker = new google.maps.Marker({
-        position: myLatLng,
-        map: map,
-        title: 'South Jakarta, INA' // Title Location
-    });
+  new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: "Sumeru city mall", // Title Location
+  });
 }
